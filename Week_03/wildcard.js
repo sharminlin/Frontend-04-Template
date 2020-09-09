43c3298cfc21a78fe7ee8d0c16ff47617c00d775
sharmin <sharminh@163.com>
2020-09-09 14:08:30 +0800

    feat(week3): wildcard

--- Week_03/wildcard.js
+++ Week_03/wildcard.js
@@ -6,7 +6,7 @@ function wildcard (source, pattern) {
   // 双指针
   let i = 0, j = 0;
 
-  while (i < source.length && j < pattern.length) {
+  while (i < source.length) {
     if (pattern[j] === '?' || pattern[j] === source[i]) {
       // ? 直接并进
       i++
@@ -15,6 +15,7 @@ function wildcard (source, pattern) {
       // 最后一个*
       if (j === pattern.length - 1) return true
       // *，表示0或多个，递归，j往后移一位，与i比，不等，与i+1比，不等，与i+2比...
+      // TODO：此处可用KMP优化
       for (let k = i; k < source.length; k++) {
         if (wildcard(source.slice(k), pattern.slice(j+1))) {
           return true
@@ -26,9 +27,13 @@ function wildcard (source, pattern) {
     }
   }
 
-  return false
+  // i走完了，看看j尾部是否都是*，存在非*表示false
+  while (j < pattern.length) {
+    if (pattern[j] !== '*') return false
+    j++
+  }
+
+  return true
 }
 
-// todo 尾部还有问题
-
-console.log(wildcard('abcdefg', 'a*c*?*g*'))
+console.log(wildcard('abcdefg', 'a*c***g*'))
