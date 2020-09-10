d1fe894a7a50cf8bf25a85586d8c5b65276318bf
sharmin <sharminh@163.com>
2020-09-10 18:12:17 +0800

    fix(kmp)

--- Week_03/kmp.js
+++ Week_03/kmp.js
@@ -24,6 +24,9 @@ function match(source, pattern) {
   return -1
 }
 
+/**
+ * 首尾匹配，匹配成功双进，j的位置表示后缀能匹配多少个前缀
+ */
 function getPmt (pattern) {
   const pmt = new Array(pattern.length).fill(0)
 
@@ -31,18 +34,21 @@ function getPmt (pattern) {
   let i = 1, j = 0;
   while (i < pattern.length) {
     if (pattern[i] === pattern[j]) {
+      // 匹配成功，进位，当前值为j的位置，表示可匹配j+1个前缀
       i++;
       j++;
+      // 此处pmt[i]记录的是i之前的，不包括i的字符串噢
       pmt[i] = j;
     } else {
       if (j === 0) {
+        // 0处不匹配，i往后走
         i++
       } else {
-        j = pmt[j]
+        j = pmt[j] // 此处不是回退至0，因为j之前的字符串可能存在相同后缀数，比如aab，如果j在b处，回退处应该是1。测试：aabaaac
       }
     }
   }
   return pmt
 }
 
-console.log(match('abcdabc abcdabce', 'abcdabce'))
+console.log(match("aabaar aabaaac","aabaaac"))
