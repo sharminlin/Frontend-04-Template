edb275cce6dd3f50187bc986d818de51977d4dcf
sharmin <sharminh@163.com>
2020-09-25 11:53:28 +0800

    fix(week 05)

--- Week_05/conversion.js
+++ Week_05/conversion.js
@@ -4,7 +4,27 @@
 // - 10进制 `10`、`.1`、`10.`、`1e2`  
 // - 16进制 `0x`
 
-function StringToNumber (str) {}
+function StringToNumber (str) {
+  const prefix = str.slice(0, 2).toLowerCase()
+  const radixMap = {
+    '0b': 2,
+    '0o': 8,
+    '0x': 16
+  }
+  if (radixMap[prefix]) {
+    return parseInt(str.slice(2), radixMap[prefix])
+  } else {
+    return Number(str)
+  }
+}
 
 // 进制
-function NumberToString(pattern) {}
+function NumberToString(number, radix = 10) {
+  const radixMap = {
+    '2': '0b',
+    '8': '0o',
+    '10': '',
+    '16': '0x'
+  }
+  return (radixMap[radix] === undefined ? '' : radixMap[radix]) + Number(number).toString(radix)
+}
