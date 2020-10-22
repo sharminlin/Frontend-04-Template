edc9530b104a2b52bb4d83ca6822f785a5c2ac8b
sharmin <sharminh@163.com>
2020-10-22 15:35:06 +0800

    feat(week 08)

---
+++ Week_08/brower/server.js
@@ -0,0 +1,31 @@
+const http = require('http')
+
+http.createServer((request, response) => {
+  let body = []
+  request.on('error', err => {
+    console.error(err)
+  }).on('data', chunk => {
+    body.push(chunk.toString())
+  }).on('end', () => {
+    // body = Buffer.concat(body).toString()
+    console.log('body: ' + body)
+    response.writeHead(200, { 'Content-Type': 'text/html' })
+    response.end(`
+      <html p="myhtml" >
+        <head>
+          <style>
+            html, body, #app {
+              height: 100%;
+              background-color: #fff
+            }
+          </style>
+        </head>
+        <body>
+          <div id="app"></div>
+        </body>
+      </html>
+    `)
+  })
+}).listen(8081)
+
+console.log('server started on http://localhost:8081')
